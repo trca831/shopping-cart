81929cac0079085f5ae970aceb4307fdb717b02e
Tracy Cano <tracycano3.14@gmail.com>
2024-02-07 19:51:02 -0800

    adding cart files for index, new, show, controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,4 +2,17 @@ class CartController < ApplicationController
     def index
         @carts = Cart.all
     end
+
+    def show
+        @cart = Cart.find(params[:id])
+    end
+
+    def new
+        @cart = Cart.new
+    end
+
+    def create
+       
+       render plain: params.class
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :products
+  resources :carts
 
   get '/cart', to: 'cart#index'
+  post '/cart', to: 'cart#create'
+  get '/cart/new', to: 'cart#new', as: "new_Cart"
+  get '/cart/:id', to: 'cart#show', as: 'Cart'
+  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
